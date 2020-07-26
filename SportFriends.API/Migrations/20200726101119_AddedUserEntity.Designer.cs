4be5f3ec785b8b6dfbefac67da43c3b3b4b8a25f
codeNut247 <codeNut247@gmail.com>
2020-07-26 12:53:55 +0200

    User Model added

--- SportFriends.API/Migrations/20200726101119_AddedUserEntity.Designer.cs
+++ SportFriends.API/Migrations/20200726101119_AddedUserEntity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportFriends.API.Data;
 
 namespace SportFriends.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200726101119_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -22,7 +24,7 @@ namespace SportFriends.API.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
-                    b.Property<string>("Name")
+                    b.Property<string>("UserName")
                         .HasColumnType("TEXT");
 
                     b.Property<byte[]>("PasswordHash")
