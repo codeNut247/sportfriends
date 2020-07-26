c2bf4e088e510a0086aa467af9935a50da31958e
codeNut247 <codeNut247@gmail.com>
2020-07-26 12:54:27 +0200

    Authentication Repository Interface + Impl.

---
+++ SportFriends.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using SportFriends.API.Models;
+
+namespace SportFriends.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
